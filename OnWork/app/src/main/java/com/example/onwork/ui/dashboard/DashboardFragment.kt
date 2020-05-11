682ef30ec0d90480b3e79ff678034fea04fff3aa
Karaerk <kerim-karaer@hotmail.com>
2020-05-11 21:24:25 +0200

    Update dashboard's design to add missing parts

--- OnWork/app/src/main/java/com/example/onwork/ui/dashboard/DashboardFragment.kt
+++ OnWork/app/src/main/java/com/example/onwork/ui/dashboard/DashboardFragment.kt
@@ -1,10 +1,8 @@
 package com.example.onwork.ui.dashboard
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
@@ -13,12 +11,18 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.onwork.R
 import com.example.onwork.model.TimeEntry
 import kotlinx.android.synthetic.main.fragment_dashboard.*
+import kotlinx.android.synthetic.main.item_time_entry.*
 
 class DashboardFragment : Fragment() {
 
     private lateinit var activityContext: AppCompatActivity
     private val timeEntryAdapter =
-        TimeEntryAdapter(arrayListOf(TimeEntry("test"),TimeEntry("test"))) { timeEntry: TimeEntry ->
+        TimeEntryAdapter(
+            arrayListOf(
+                TimeEntry("test"),
+                TimeEntry("test")
+            )
+        ) { timeEntry: TimeEntry ->
             timeEntryClicked(
                 timeEntry
             )
@@ -34,6 +38,8 @@ class DashboardFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setHasOptionsMenu(true)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -49,11 +55,16 @@ class DashboardFragment : Fragment() {
      * Prepares all the views inside this fragment.
      */
     private fun initViews() {
-        rvTimeEntries.layoutManager = LinearLayoutManager(activityContext, RecyclerView.VERTICAL, false)
+        rvTimeEntries.layoutManager =
+            LinearLayoutManager(activityContext, RecyclerView.VERTICAL, false)
         rvTimeEntries.adapter = timeEntryAdapter
+
+        tvDate.text = getString(R.string.title_on_going)
+        tvTime.text = getString(R.string.label_start_time, "10:45")
 
         fabEntry.setOnClickListener {
             fabEntry.setImageDrawable(activityContext.getDrawable(R.drawable.ic_stop_white_24dp))
+            iOnGoing.visibility = View.VISIBLE
         }
     }
 
@@ -62,5 +73,21 @@ class DashboardFragment : Fragment() {
      */
     private fun timeEntryClicked(timeEntry: TimeEntry) {
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        inflater.inflate(R.menu.menu_dashboard, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_delete_history -> true
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
